0b0ce1ab102015ec75ea4b3ab0ed6cd46eccabfc
lain <145809470+Ivulai@users.noreply.github.com>
2023-12-16 22:31:03 +0700

    added main function

--- rps.js
+++ rps.js
@@ -1,4 +1,6 @@
 // 0 == rock, 1 == scissors, 2 == paper
+let playerScore = 0
+let computerScore = 0
 function getPlayerChoice(chosen) {
     chosen = chosen.toLowerCase()
     if (chosen == "rock") {
@@ -29,33 +31,43 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return 'win'
     } 
     else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore++;
         return 'win'
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return 'win'
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore++;
         return 'lose'
     }
     else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore++;
         return 'lose'
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore++;
         return 'lose'}
     else {
         return 'draw'
-    }
-
+    };
 };
 
-let playerPrompt = prompt("choose a fighter: rock, paper, scissors");
+function game() {
+    for (let i = 1; i <= 5 ; i++){
+        let playerPrompt = prompt("choose a fighter: rock, paper, scissors");
+        const playerSelection = getPlayerChoice(playerPrompt);
+        const computerSelection = getComputerChoice();
+        console.log("You choose " + playerSelection + " bot choose " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("The score is you: " + playerScore + " bot is " + computerScore)
+    }
+}
 
-const playerSelection = getPlayerChoice(playerPrompt);
-const computerSelection = getComputerChoice();
+console.log(game())
 
-console.log(playRound(playerSelection))
-
-console.log(getComputerChoice())
